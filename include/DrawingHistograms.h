31d2cf923ed12a4fab652c24aa8e1cd6cb307fcb
Schuea <anne.schuetz@desy.de>
2015-11-30 13:32:17 +0100

    Implemented functions for initializing SubDetector classes

--- include/DrawingHistograms.h
+++ include/DrawingHistograms.h
@@ -1,6 +1,36 @@
 #ifndef DRAWINGHISTOS
 #define DRAWINGHISTOS
 
+#include "TFile.h"
+#include "TAxis.h"
+#include "TBrowser.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TF1.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TGraphErrors.h"
+#include "TStyle.h"
+#include "TPaveText.h"
+#include "TLatex.h"
+#include "TEllipse.h"
+#include "TPaveStats.h"
+#include "TProfile.h"
+#include "TMath.h"
+#include "TChain.h"
+#include "TROOT.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <array>
+
+#include <map>
+
 #include "CreateCellID.h"
 #include "Subdetector.h"
 #include "LayerCodeInCellID.h"
@@ -20,153 +50,48 @@ void DrawingMacro(string outputname, std::vector<std::string> inputnames, std::v
 				std::string subdetector_name;
 				stringstream several_subdetector_names;
 
-				std::vector<Subdetector*> SubDetectors;
+				std::vector<Subdetector*> * SubDetectors;
 				for (size_t s = 0; s < argument_subdetectors.size(); ++s) {
 								if (argument_subdetectors.size() == 1) {
 												several_subdetector_names.str("");
 												several_subdetector_names << argument_subdetectors.at(0);
+
 												if (argument_subdetectors.at(0) == std::string("allCalo")) {
-																Subdetector* SubCalo1;
-																Subdetector* SubCalo2;
-																Subdetector* SubCalo3;
-																Subdetector* SubCalo4;
-																Subdetector* SubCalo5;
-																Subdetector* SubCalo6;
-																Subdetector* SubCalo7;
-																Subdetector* SubCalo8;
 
-																InitializeAllCaloSubdetectors(SubCalo1,SubCalo2,SubCalo3,SubCalo4,SubCalo5,SubCalo6,SubCalo7,SubCalo8);
-
-																SubDetectors.push_back(SubCalo1);
-																SubDetectors.push_back(SubCalo2);
-																SubDetectors.push_back(SubCalo3);
-																SubDetectors.push_back(SubCalo4);
-																SubDetectors.push_back(SubCalo5);
-																SubDetectors.push_back(SubCalo6);
-																SubDetectors.push_back(SubCalo7);
-																SubDetectors.push_back(SubCalo8);
+																SubDetectors->clear();
+																InitializeAllCaloSubdetectors(SubDetectors);
 												}
 												if (argument_subdetectors.at(s) == std::string("allTracker")) {
 																several_subdetector_names.str("");
 																several_subdetector_names << argument_subdetectors.at(s);
 
-																SubDetectors.clear();
-																Subdetector* SubTracker1;
-																Subdetector* SubTracker2;
-																Subdetector* SubTracker3;
-																Subdetector* SubTracker4;
-																Subdetector* SubTracker5;
-
-																InitializeAllTrackerSubdetectors(SubTracker1,SubTracker2,SubTracker3,SubTracker4,SubTracker5);
-
-																SubDetectors.push_back(SubTracker1);
-																SubDetectors.push_back(SubTracker2);
-																SubDetectors.push_back(SubTracker3);
-																SubDetectors.push_back(SubTracker4);
-																SubDetectors.push_back(SubTracker5);
+																SubDetectors->clear();
+																InitializeAllTrackerSubdetectors(SubDetectors);
 												}
 												if (argument_subdetectors.at(s) == std::string("all")) {
 																several_subdetector_names.str("");
 																several_subdetector_names << argument_subdetectors.at(s);
 
-																SubDetectors.clear();
-																Subdetector* SubCalo1;
-																Subdetector* SubCalo2;
-																Subdetector* SubCalo3;
-																Subdetector* SubCalo4;
-																Subdetector* SubCalo5;
-																Subdetector* SubCalo6;
-																Subdetector* SubCalo7;
-																Subdetector* SubCalo8;
-																Subdetector* SubTracker1;
-																Subdetector* SubTracker2;
-																Subdetector* SubTracker3;
-																Subdetector* SubTracker4;
-																Subdetector* SubTracker5;
-
-																InitializeAllSubdetectors(SubCalo1,SubCalo2,SubCalo3,SubCalo4,SubCalo5,SubCalo6,SubCalo7,SubCalo8,SubTracker1,SubTracker2,SubTracker3,SubTracker4,SubTracker5);
-
-																SubDetectors.push_back(SubCalo1);
-																SubDetectors.push_back(SubCalo2);
-																SubDetectors.push_back(SubCalo3);
-																SubDetectors.push_back(SubCalo4);
-																SubDetectors.push_back(SubCalo5);
-																SubDetectors.push_back(SubCalo6);
-																SubDetectors.push_back(SubCalo7);
-																SubDetectors.push_back(SubCalo8);
-																SubDetectors.push_back(SubTracker1);
-																SubDetectors.push_back(SubTracker2);
-																SubDetectors.push_back(SubTracker3);
-																SubDetectors.push_back(SubTracker4);
-																SubDetectors.push_back(SubTracker5);
-
+																SubDetectors->clear();
+																InitializeAllSubdetectors(SubDetectors);
 												} else {
-																Subdetector* SubDetector;
-																InitializeWhichSubdetector(argument_subdetectors.at(0));
-																SubDetectors.push_back(SubDetector);
+																InitializeWhichSubdetector(argument_subdetectors.at(0), SubDetectors);
 												}
 								} else {
 												if (argument_subdetectors.at(s) == std::string("all")) {
 																several_subdetector_names.str("");
 																several_subdetector_names << argument_subdetectors.at(s);
 
-																SubDetectors.clear();
-																Subdetector* SubCalo1;
-																Subdetector* SubCalo2;
-																Subdetector* SubCalo3;
-																Subdetector* SubCalo4;
-																Subdetector* SubCalo5;
-																Subdetector* SubCalo6;
-																Subdetector* SubCalo7;
-																Subdetector* SubCalo8;
-																Subdetector* SubTracker1;
-																Subdetector* SubTracker2;
-																Subdetector* SubTracker3;
-																Subdetector* SubTracker4;
-																Subdetector* SubTracker5;
-
-																InitializeAllSubdetectors(SubCalo1,SubCalo2,SubCalo3,SubCalo4,SubCalo5,SubCalo6,SubCalo7,SubCalo8,SubTracker1,SubTracker2,SubTracker3,SubTracker4,SubTracker5);
-
-																SubDetectors.push_back(SubCalo1);
-																SubDetectors.push_back(SubCalo2);
-																SubDetectors.push_back(SubCalo3);
-																SubDetectors.push_back(SubCalo4);
-																SubDetectors.push_back(SubCalo5);
-																SubDetectors.push_back(SubCalo6);
-																SubDetectors.push_back(SubCalo7);
-																SubDetectors.push_back(SubCalo8);
-																SubDetectors.push_back(SubTracker1);
-																SubDetectors.push_back(SubTracker2);
-																SubDetectors.push_back(SubTracker3);
-																SubDetectors.push_back(SubTracker4);
-																SubDetectors.push_back(SubTracker5);
-
+																SubDetectors->clear();
+																InitializeAllSubdetectors(SubDetectors);
 																break;
 												}
 												if (argument_subdetectors.at(s) == std::string("allCalo")) {
 																several_subdetector_names.str("");
 																several_subdetector_names << argument_subdetectors.at(s);
 
-																SubDetectors.clear();
-																Subdetector* SubCalo1;
-																Subdetector* SubCalo2;
-																Subdetector* SubCalo3;
-																Subdetector* SubCalo4;
-																Subdetector* SubCalo5;
-																Subdetector* SubCalo6;
-																Subdetector* SubCalo7;
-																Subdetector* SubCalo8;
-
-																InitializeAllCaloSubdetectors(SubCalo1,SubCalo2,SubCalo3,SubCalo4,SubCalo5,SubCalo6,SubCalo7,SubCalo8);
-
-																SubDetectors.push_back(SubCalo1);
-																SubDetectors.push_back(SubCalo2);
-																SubDetectors.push_back(SubCalo3);
-																SubDetectors.push_back(SubCalo4);
-																SubDetectors.push_back(SubCalo5);
-																SubDetectors.push_back(SubCalo6);
-																SubDetectors.push_back(SubCalo7);
-																SubDetectors.push_back(SubCalo8);
+																SubDetectors->clear();
+																InitializeAllCaloSubdetectors(SubDetectors);
 
 																if (argument_subdetectors.at(s+1) == std::string("allTracker")){
 																				std::cerr << "If you want to compare all subdetectors, just use the argument 'all' after the flag -s!" << std::endl;
@@ -178,20 +103,9 @@ void DrawingMacro(string outputname, std::vector<std::string> inputnames, std::v
 																several_subdetector_names.str("");
 																several_subdetector_names << argument_subdetectors.at(s);
 
-																SubDetectors.clear();
-																Subdetector* SubTracker1;
-																Subdetector* SubTracker2;
-																Subdetector* SubTracker3;
-																Subdetector* SubTracker4;
-																Subdetector* SubTracker5;
+																SubDetectors->clear();
+																InitializeAllTrackerSubdetectors(SubDetectors);
 
-																InitializeAllTrackerSubdetectors(SubTracker1,SubTracker2,SubTracker3,SubTracker4,SubTracker5);
-
-																SubDetectors.push_back(SubTracker1);
-																SubDetectors.push_back(SubTracker2);
-																SubDetectors.push_back(SubTracker3);
-																SubDetectors.push_back(SubTracker4);
-																SubDetectors.push_back(SubTracker5);
 																if (argument_subdetectors.at(s+1) == std::string("allCalo")){
 																				std::cerr << "If you want to compare all subdetectors, just use the argument 'all' after the flag -s!" << std::endl;
 																				terminate();
@@ -200,9 +114,7 @@ void DrawingMacro(string outputname, std::vector<std::string> inputnames, std::v
 
 												} else {
 																several_subdetector_names << argument_subdetectors.at(s);
-																Subdetector* SubDetector;
-																InitializeWhichSubdetector(argument_subdetectors.at(s));
-																SubDetectors.push_back(SubDetector);
+																InitializeWhichSubdetector(argument_subdetectors.at(s), SubDetectors);
 												}
 								}
 				}
@@ -216,67 +128,41 @@ void DrawingMacro(string outputname, std::vector<std::string> inputnames, std::v
 
 
 }
-void InitializeAllSubdetectors(SubCalo1,SubCalo2,SubCalo3,SubCalo4,SubCalo5,SubCalo6,SubCalo7,SubCalo8,SubTracker1,SubTracker2,SubTracker3,SubTracker4,SubTracker5){
-				InitializeAllCaloSubdetectors(SubCalo1,SubCalo2,SubCalo3,SubCalo4,SubCalo5,SubCalo6,SubCalo7,SubCalo8);
-				InitializeAllTrackerSubdetectors(SubTracker1,SubTracker2,SubTracker3,SubTracker4,SubTracker5);
+void InitializeAllSubdetectors(std::vector< Subdetector* > * SubDetectors){
+				InitializeAllCaloSubdetectors(SubDetectors);
+				InitializeAllTrackerSubdetectors(SubDetectors);
 }				
-void InitializeAllCaloSubdetectors(SubCalo1,SubCalo2,SubCalo3,SubCalo4,SubCalo5,SubCalo6,SubCalo7,SubCalo8){
-				SubCalo1 = new EcalBarrel();
-				SubCalo2 = new EcalEndcap();
-				SubCalo3 = new HcalBarrel();
-				SubCalo4 = new HcalEndcap();
-				SubCalo5 = new MuonBarrel();
-				SubCalo6 = new MuonEndcap();
-				SubCalo7 = new BeamCal();
-				SubCalo8 = new LumiCal();
+void InitializeAllCaloSubdetectors(std::vector< Subdetector* > * SubDetectors){
+				SubDetectors->push_back(new EcalBarrel());
+				SubDetectors->push_back(new EcalEndcap());
+				SubDetectors->push_back(new HcalBarrel());
+				SubDetectors->push_back(new HcalEndcap());
+				SubDetectors->push_back(new MuonBarrel());
+				SubDetectors->push_back(new MuonEndcap());
+				SubDetectors->push_back(new BeamCal());
+				SubDetectors->push_back(new LumiCal());
 }
-void InitializeAllTrackerSubdetectors(SubTracker1,SubTracker2,SubTracker3,SubTracker4,SubTracker5){
-				SubTracker1 = new SiVertexBarrel();
-				SubTracker2 = new SiVertexEndcap();
-				SubTracker3 = new SiTrackerBarrel();
-				SubTracker4 = new SiTrackerEndcap();
-				SubTracker5 = new SiTrackerForward();
-}
-void InitializeWhichSubdetector(std::string SubdetectorName){
-				if (SubdetectorName == std::string("EcalBarrel")){
-								SubDetector = new EcalBarrel();
-				}
-				if (SubdetectorName == std::string("EcalEndcap")){
-								SubDetector = new EcalEndcap();
-				}
-				if (SubdetectorName == std::string("HcalBarrel")){
-								SubDetector = new HcalBarrel();
-				}
-				if (SubdetectorName == std::string("HcalEndcap")){
-								SubDetector = new HcalEndcap();
-				}
-				if (SubdetectorName == std::string("MuonBarrel")){
-								SubDetector = new MuonBarrel();
-				}
-				if (SubdetectorName == std::string("MuonEndcap")){
-								SubDetector = new MuonEndcap();
-				}
-				if (SubdetectorName == std::string("BeamCal")){
-								SubDetector = new BeamCal();
-				}
-				if (SubdetectorName == std::string("LumiCal")){
-								SubDetector = new LumiCal();
-				}
-				if (SubdetectorName == std::string("SiVertexBarrel")){
-								SubDetector = new SiVertexBarrel();
-				}
-				if (SubdetectorName == std::string("SiVertexEndcap")){
-								SubDetector = new SiVertexEndcap();
-				}
-				if (SubdetectorName == std::string("SiTrackerBarrel")){
-								SubDetector = new SiTrackerBarrel();
-				}
-				if (SubdetectorName == std::string("SiTrackerEndcap")){
-								SubDetector = new SiTrackerEndcap();
-				}
-				if (SubdetectorName == std::string("SiTrackerForward")){
-								SubDetector = new SiTrackerForward();
-				}
+void InitializeAllTrackerSubdetectors(std::vector< Subdetector* > * SubDetectors){
+				SubDetectors->push_back(new SiVertexBarrel());
+				SubDetectors->push_back(new SiVertexEndcap());
+				SubDetectors->push_back(new SiTrackerBarrel());
+				SubDetectors->push_back(new SiTrackerEndcap());
+				SubDetectors->push_back(new SiTrackerForward());
+}       
+void InitializeWhichSubdetector(std::string SubdetectorName, std::vector< Subdetector* > * SubDetectors){
+				if (SubdetectorName == std::string("EcalBarrel")) SubDetectors->push_back(new EcalBarrel());
+				if (SubdetectorName == std::string("EcalEndcap")) SubDetectors->push_back(new EcalEndcap());
+				if (SubdetectorName == std::string("HcalBarrel")) SubDetectors->push_back(new HcalBarrel());
+				if (SubdetectorName == std::string("HcalEndcap")) SubDetectors->push_back(new HcalEndcap());
+				if (SubdetectorName == std::string("MuonBarrel")) SubDetectors->push_back(new MuonBarrel());
+				if (SubdetectorName == std::string("MuonEndcap")) SubDetectors->push_back(new MuonEndcap());
+				if (SubdetectorName == std::string("BeamCal"))    SubDetectors->push_back(new BeamCal());
+				if (SubdetectorName == std::string("LumiCal"))    SubDetectors->push_back(new LumiCal());
+				if (SubdetectorName == std::string("SiVertexBarrel"))   SubDetectors->push_back(new SiVertexBarrel());
+				if (SubdetectorName == std::string("SiVertexEndcap"))   SubDetectors->push_back(new SiVertexEndcap());
+				if (SubdetectorName == std::string("SiTrackerBarrel"))  SubDetectors->push_back(new SiTrackerBarrel());
+				if (SubdetectorName == std::string("SiTrackerEndcap"))  SubDetectors->push_back(new SiTrackerEndcap());
+				if (SubdetectorName == std::string("SiTrackerForward")) SubDetectors->push_back(new SiTrackerForward());
 }
 void Setup_BinningArrays(std::vector<CaloIDs> SubDetectors, std::array<int, 3> *axis_range_plot_1D,
 								std::array<float, 3> *axis_range_plot_energy_1D, std::array<int, 9> *axis_ranges_plot) {
