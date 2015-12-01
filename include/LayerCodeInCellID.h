54c5f3a8913d3b7ca11f50a2b7da14b373e15b59
Schuea <anne.schuetz@desy.de>
2015-12-01 18:14:53 +0100

    Put the functions into the cpp file

--- include/LayerCodeInCellID.h
+++ include/LayerCodeInCellID.h
@@ -10,16 +10,8 @@ class LayerCodeInCellID {
 
     virtual ~LayerCodeInCellID() {}
 
-    int GetLayer() const {
-      if (Layer >= 0){
-        return FindLayer(CellID_, StarBin_layers, LengthBin_layers);
-      }
-      else throw std::runtime_error("You didn't initialize the class with an ID!");
-    }				
-    int ObtainLayerfromCellID(int const cellid,  int const StarBin_layers, int const LengthBin_layers) const{
-	std::bitset<64> cellidbit = std::bitset<std::numeric_limits<unsigned int>::digits>(cellid);
-	return FindLayer(cellidbit, StarBin_layers, LengthBin_layers);
-    }
+    int GetLayer() const;				
+    int ObtainLayerfromCellID(int const cellid,  int const StarBin_layers, int const LengthBin_layers) const;
 
   private:
     LayerCodeInCellID() {}
--- src/LayerCodeInCellID.cpp
+++ src/LayerCodeInCellID.cpp
@@ -14,3 +14,16 @@ int LayerCodeInCellID::FindLayer(std::string const CellID_, int const StarBin_la
 
   return Layer;
 }
+
+int LayerCodeInCellID::GetLayer() const {
+  if (Layer >= 0){
+    return FindLayer(CellID_, StarBin_layers, LengthBin_layers);
+  }
+  else throw std::runtime_error("You didn't initialize the class with an ID!");
+}				
+
+int LayerCodeInCellID::ObtainLayerfromCellID(int const cellid,  int const StarBin_layers, int const LengthBin_layers) const{
+  std::bitset<64> cellidbit = std::bitset<std::numeric_limits<unsigned int>::digits>(cellid);
+  return FindLayer(cellidbit, StarBin_layers, LengthBin_layers);
+}
+
