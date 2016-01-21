0c9b34d8ce5fab32ad2dd50c3483e632b29404b1
Schuea <anne.schuetz@desy.de>
2016-01-21 15:59:38 +0100

    Struct to find a pair by the first entry

--- include/UsefulFunctions.h
+++ include/UsefulFunctions.h
@@ -1,6 +1,8 @@
 #ifndef USEFULFUNCTIONS
 #define USEFULFUNCTIONS
 
+#include <utility>
+
 float FindMax(float const value, float max);
 float FindMax(int const value, float max);
 float FindMax(float const value, int max);
@@ -11,4 +13,14 @@ float FindMin(float const value, int min);
 float FindMin(int const value, float min);
 int FindMin(int const value, int min);
 
+struct CompareFirst
+{
+  CompareFirst(int val) : val_(val) {}
+
+  bool operator()(const std::pair< int,int >& elem) const {
+    return val_ == elem.first;
+  }
+  private:
+    int val_;
+};
 #endif
